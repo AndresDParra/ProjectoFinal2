1f513ea02d412d554f7f4824e39a2ae3df717c80
Andres David Parra <andresdavidparra@Andress-Laptop-2.local>
2025-04-03 14:29:19 -0500

    Records listos

--- src/main/java/com/example/projectofinal2/Clases/model/Transaccion.java
+++ src/main/java/com/example/projectofinal2/Clases/model/Transaccion.java
@@ -2,38 +2,6 @@ package com.example.projectofinal2.Clases.model;
 
 import java.time.LocalDateTime;
 
-public class Transaccion {
-    private final double monto;
-    private final LocalDateTime fecha;
-    private final String descripcion;
-    private final CategoriaTransaccion categoria;
-    private final TipoTransaccion tipo;
-
-    public Transaccion(LocalDateTime fecha, double monto, String descripcion, CategoriaTransaccion categoria,TipoTransaccion tipo) {
-        this.fecha = fecha;
-        this.monto = monto;
-        this.descripcion = descripcion;
-        this.categoria = categoria;
-        this.tipo = tipo;
-    }
-
-    public double getMonto() {
-        return monto;
-    }
-
-    public LocalDateTime getFecha() {
-        return fecha;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public CategoriaTransaccion getCategoria() {
-        return categoria;
-    }
-
-    public TipoTransaccion getTipo() {
-        return tipo;
-    }
+public record Transaccion(LocalDateTime fecha, double monto, String descripcion, CategoriaTransaccion categoria,
+                          TipoTransaccion tipo) {
 }
